47711f2d03669ac40fdaced6e224c8d91e517dc1
“saenuruki” <sae.webwork@gmail.com>
2020-04-03 20:29:55 +0900

    add binary and array

---
+++ binaryGap.py
@@ -0,0 +1,47 @@
+#A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+#
+#For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+#
+#Write a function:
+#
+#def solution(N)
+#
+#that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+#
+#For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+#
+#Write an efficient algorithm for the following assumptions:
+#
+#N is an integer within the range [1..2,147,483,647].
+
+
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+import math
+
+def solution(N):
+    # write your code in Python 3.6
+    # Nを2進数に変換する
+    # 2進数の1と1の間の0の数をカウントする -> countに格納する
+    # 最終的なcountをresultに格納する
+    # return result
+    
+    binaryN = int(format(N, 'b'))
+    result = 0
+    count = 0
+    flag = 0
+
+    while binaryN != 0:
+        target = binaryN % 10
+        binaryN = math.floor(binaryN / 10)
+
+        if target == 1:
+            flag = 1
+            if count >= result:
+                result = count
+            count = 0
+        elif target == 0:
+            if flag == 1:
+                count = count + 1
+    
+    return result
